b90aadaf23fd664dbbe7e6975feeef743304114c
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-03-23 12:11:38 +0300

    change buffer movements from shift to alt for consistency with default vim motions(already used)

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -35,8 +35,10 @@ map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- buffers
-map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+--map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+--map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<A-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+map("n", "<A-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
