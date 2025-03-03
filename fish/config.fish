96ec6afb7c44af8ba196a5853d54ed1d74468a35
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-03-03 11:49:06 +0300

    some fish fixes

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,9 @@
 # Only run this in interactive shells
 if status is-interactive
+    source $HOME/.config/fish/settings.fish
 
-    # I'm trying to grow a neckbeard
-    # fish_vi_key_bindings
-    # Set the cursor shapes for the different vi modes.
-    set fish_cursor_default block
-    set fish_cursor_insert block
-    set fish_cursor_replace_one underscore
-    set fish_cursor_visual block
-
-    fish_set_user_paths
-    fish_user_key_bindings
 end
 
-source $HOME/.config/fish/settings.fish
 source $HOME/.config/fish/env.vars.fish
 
 fzf --fish | source
--- fish/settings.fish
+++ fish/settings.fish
@@ -1,7 +1,10 @@
-# set cursor shape to block for everything
-#fish_vi_cursor
-#set fish_cursor_default block
-#set fish_cursor_insert block
-#set fish_cursor_visual block
-# Set cursor to block on Fish startup
-#echo -ne "\e[2 q"
+# I'm trying to grow a neckbeard
+# fish_vi_key_bindings
+# Set the cursor shapes for the different vi modes.
+set fish_cursor_default block
+set fish_cursor_insert block
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
+fish_set_user_paths
+fish_user_key_bindings
