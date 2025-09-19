e0a182528388967a3372c3c1aa15c065b796d3c7
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2025-09-19 11:00:11 +0300

    add fzf-fish ?

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -9,20 +9,4 @@ function fish_user_key_bindings
     # Bind Ctrl+F to run tmux-sessionizer (equivalent to bindkey -s '^f' "tmux-sessionizer\n")
     bind \cf tmux-sessionizer
     bind -M insert \cf tmux-sessionizer 'commandline -f repaint'
-
-    # Ctrl-n: next (pager-aware / history / cursor forward)
-    bind \cn forward-char
-
-    # Ctrl-p: previous (pager-aware / history / cursor backward)
-    bind \cp backward-char
-
-    # Ctrl-d: page down in pager (fallback: down one line, or delete-char-or-exit if no pager)
-    function __pager_page_down --description 'page down in pager or delete-char-or-exit'
-        if status --is-interactive
-            commandline -f page-down 2>/dev/null || commandline -f down-line 2>/dev/null
-        end
-    end
-    bind \cd __pager_page_down
-
-
 end
