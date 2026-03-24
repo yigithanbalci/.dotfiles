d5458d86ba37abf1aa7b5679833472d75c85434f
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-24 15:13:30 +0300

    remove gitlog and gitrebase aliases since there are in gitconfig anyways

--- zsh-conf/conf.d/05-aliases.zsh
+++ zsh-conf/conf.d/05-aliases.zsh
@@ -17,5 +17,3 @@ fi
 
 alias emacs='emacsclient -c -a ""'
 alias jvim="nvim --cmd 'set runtimepath^=\$HOME/.config/jvim'"
-alias gitlog='git log --oneline --graph --decorate --all'
-alias gitrebase='git rebase --interactive --autostash --keep-empty --no-autosquash --rebase-merges'
