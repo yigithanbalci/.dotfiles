9e1cab769ba614b2e8927d2ae40500e76919eff0
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-29 11:35:49 +0300

    add suffix aliases to open files by names directly in program specified

---
+++ zsh-conf/conf.d/08-suffix_aliases.zsh
@@ -0,0 +1,13 @@
+# Suffix Aliases - Open Files by Extension
+# Just type the filename to open it with the associated program
+alias -s json=jless
+alias -s md=bat
+alias -s go='$EDITOR'
+alias -s rs='$EDITOR'
+alias -s txt=bat
+alias -s log=bat
+alias -s py='$EDITOR'
+alias -s js='$EDITOR'
+alias -s ts='$EDITOR'
+alias -s toml='$EDITOR'
+alias -s html=open  # macOS: open in default browser
