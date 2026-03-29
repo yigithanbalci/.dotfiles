e5e938ee93f714bc1bc2103252965d37c18e5ad7
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-29 11:53:55 +0300

    tidy suffix aliases

--- zsh-conf/conf.d/08-suffix_aliases.zsh
+++ zsh-conf/conf.d/08-suffix_aliases.zsh
@@ -1,13 +1,43 @@
-# Suffix Aliases - Open Files by Extension
+# Suffix Aliases - Open files by extension
 # Just type the filename to open it with the associated program
+#   e.g.  README.md  ->  bat README.md
+#         main.go    ->  $EDITOR main.go
+
+# --- Viewer: bat (paged, syntax-highlighted) ---
+alias -s {md,txt,log}=bat
+alias -s {yaml,yml,xml,csv,sql}=bat
+alias -s {properties,gitignore,gitconfig,editorconfig}=bat
+
+# --- Viewer: jless (interactive JSON) ---
 alias -s json=jless
-alias -s md=bat
-alias -s go='$EDITOR'
-alias -s rs='$EDITOR'
-alias -s txt=bat
-alias -s log=bat
-alias -s py='$EDITOR'
-alias -s js='$EDITOR'
-alias -s ts='$EDITOR'
-alias -s toml='$EDITOR'
-alias -s html=open  # macOS: open in default browser
+
+# --- Editor ($EDITOR / neovim) ---
+# C / C++
+alias -s {c,h,cpp,hpp,cc,hh}='$EDITOR'
+
+# Go / Rust / Zig
+alias -s {go,rs,zig}='$EDITOR'
+
+# Java / Kotlin / Gradle
+alias -s {java,kt,kts,gradle}='$EDITOR'
+
+# JavaScript / TypeScript (NestJS, React)
+alias -s {js,jsx,mjs,cjs,ts,tsx}='$EDITOR'
+
+# Python / Ruby / Dart / OCaml
+alias -s {py,rb,dart,ml,mli}='$EDITOR'
+
+# Lua
+alias -s lua='$EDITOR'
+
+# Shell
+alias -s {sh,bash,zsh,fish}='$EDITOR'
+
+# Config / Build
+alias -s {toml,cmake}='$EDITOR'
+
+# Web
+alias -s {css,scss,html}='$EDITOR'
+
+# --- Browser (macOS: open) ---
+alias -s {pdf,svg}=open
