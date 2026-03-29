7807dddbbde317c167bb424e57536fba3d967469
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2026-03-29 17:17:55 +0300

    add guard for external commands to check if exist for config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,23 +4,23 @@ if status is-interactive
     fish_user_key_bindings
 end
 
-batman --export-env | source
-zoxide init fish | source
+type -q batman; and batman --export-env | source
+type -q zoxide; and zoxide init fish | source
 
 source $HOME/.config/fish/settings.fish
 source $HOME/.config/fish/env.vars.fish
 
-fzf --fish | source
+type -q fzf; and fzf --fish | source
 
-starship init fish | source
+type -q starship; and starship init fish | source
 if status is-interactive
     # Do not run fastfetch if inside Neovim jobs (check for NVIM env var)
     if not set -q NVIM
-        fastfetch
+        type -q fastfetch; and fastfetch
     end
 end
 
 # carapace
 # seems like not needed in fish that much atleast for now
-carapace init fish | source
+type -q carapace; and carapace init fish | source
 # carapace _carapace | source
